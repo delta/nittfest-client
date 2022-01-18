8ddf85aeb8fe53101652eef06312b8a37e492cdc
muhesh <vmspride7@gmail.com>
2022-01-18 12:54:21 +0530

    fix: formspage

--- lib/services/api/api_constants.dart
+++ lib/services/api/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'https://localhost:10000/';
+  static String baseUrl = 'http://localhost:10000/';
   static String auth = '${baseUrl}auth/callback/';
   static String formQuestions = '${baseUrl}form_questions';
   static String formAnswers = '${baseUrl}form_questions/submit';
